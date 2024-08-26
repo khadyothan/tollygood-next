f6e63580a514ce8469e5f909c0c6887530cfc6a0
khadyothan <kdasari1@asu.edu>
2024-08-25 18:12:46 -0700

    changed footer styling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,23 +2,29 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white w-full mt-36 py-10">
+    <footer className="bg-white text-gray-800 w-full mt-36 py-10 shadow-lg">
       <div className="container mx-auto text-center">
-        <p className="text-lg" style={{ color: "orange" }}>
+        <p className="text-lg font-semibold" style={{ color: "orange" }}>
           tollygood.com
         </p>
         <div className="mt-2">
-          <Link href="/" className="text-gray-400 hover:text-white mx-2">
+          <Link href="/" className="text-gray-600 hover:text-orange-500 mx-2">
             Home
           </Link>
-          <Link href="/about" className="text-gray-400 hover:text-white mx-2">
+          <Link
+            href="/about"
+            className="text-gray-600 hover:text-orange-500 mx-2"
+          >
             About
           </Link>
-          <Link href="/contact" className="text-gray-400 hover:text-white mx-2">
+          <Link
+            href="/contact"
+            className="text-gray-600 hover:text-orange-500 mx-2"
+          >
             Contact
           </Link>
         </div>
-        <p className="text-sm mt-4">
+        <p className="text-sm text-gray-500 mt-4">
           &copy; {new Date().getFullYear()} All rights reserved.
         </p>
       </div>
